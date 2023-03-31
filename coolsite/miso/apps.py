48ec53b61b7befde97e952543358987582ddde8b
Shugila <zhanabylovashugyla@gmail.com>
2023-03-31 21:20:01 +0600

    Update 2

--- coolsite/miso/apps.py
+++ coolsite/miso/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MisoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'miso'
+    verbose_name='Корейские товары'
--- coolsite/miso/views.py
+++ coolsite/miso/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render, redirect
 
 from .models import *
 
-menu =[{'title': "О сайте",'url_name':'about'},
-       {'title': "Добавить статью",'url_name': 'add_page'},
-       {'title': "Обратная связь",'url_name': 'contact'},
-       {'title': "Войти",'url_name': 'login'}
-]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+        ]
 
 
 def index(request):
@@ -22,13 +22,16 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'miso/about.html',{'menu':menu,'title':'О сайте'})
+    return render(request, 'miso/about.html', {'menu': menu, 'title': 'О сайте'})
+
 
 def addpage(request):
     return HttpResponse("Добавление статьи")
 
+
 def contact(request):
     return HttpResponse("Обратная связь")
+
 
 def login(request):
     return HttpResponse("Авторизация")
@@ -37,19 +40,19 @@ def login(request):
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
+
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
 
+
 def show_category(request, cat_id):
     posts = Miso.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
 
     if len(posts) == 0:
         raise Http404()
 
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Отображение по рубрикам',
         'cat_selected': cat_id,
