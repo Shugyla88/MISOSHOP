66378f32aef4d369b718e3f4e29bdcc2c53ff4f0
Shugila <zhanabylovashugyla@gmail.com>
2023-04-23 20:55:15 +0600

    Update 2

--- coolsite/miso/views.py
+++ coolsite/miso/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
@@ -75,8 +77,8 @@ def contact(request):
     return HttpResponse("Обратная связь")
 
 
-def login(request):
-    return HttpResponse("Авторизация")
+#def login(request):
+ #   return HttpResponse("Авторизация")
 
 
 def pageNotFound(request, exception):
@@ -136,3 +138,31 @@ class MisoCategory(DataMixin,ListView):
 #     }
 #
 #     return render(request, 'miso/index.html', context=context)
+
+class RegisterUser(DataMixin,CreateView):
+    form_class = RegisterUserForm
+    template_name = 'miso/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        c_def =self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'miso/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
